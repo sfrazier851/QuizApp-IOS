c9395dc5b4c14ea2c75d0610726c5287827af358
azhelle16 <mllsumulong@gmail.com>
2022-03-11 22:28:31 -0800

    initial commit
    Name    :   Maricel Louise Sumulong
    Date    :   March 11, 2022 PST
    E-mail  :   mllsumulong@gmail.com

---
+++ QuizApp-IOS/Controllers/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp-IOS
+//
+//  Created by Maricel Sumulong on 3/11/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuizApp-IOS/Models/Globals.swift
@@ -0,0 +1,8 @@
+//
+//  Globals.swift
+//  QuizApp-IOS
+//
+//  Created by Maricel Sumulong on 3/11/22.
+//
+
+import Foundation
