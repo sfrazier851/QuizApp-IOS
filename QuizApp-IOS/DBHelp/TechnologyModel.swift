154fb15a9bf36d86960b5c4d74cdd5799e828d8b
malmsteadaa <Malmsteadaa@gmail.com>
2022-03-18 12:10:22 -0400

    merge
    db

---
+++ QuizApp-IOS/DBHelp/TechnologyModel.swift
@@ -0,0 +1,19 @@
+//
+//  TechnologyModel.swift
+//  QuizApp-IOS
+//
+//  Created by admin on 3/18/22.
+//
+
+import Foundation
+enum TechnologyTitle:String, CaseIterable {
+    case Java = "Java"
+    case IOS = "IOS"
+    case Andriod = "Andriod"
+    
+}
+func initTechTable(){
+    for i in TechnologyTitle.allCases{
+        DBCRUD.initDBCRUD.createTechnology(r: i.rawValue)
+    }
+}
