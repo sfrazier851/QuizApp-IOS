115288dc61967e19437c2fbb15b1fc198b8338b0
malmsteadaa <Malmsteadaa@gmail.com>
2022-03-19 01:04:06 -0400

    fix login binding, catch a bug with duplicates

--- QuizApp-IOS/DBHelp/UserModels.swift
+++ QuizApp-IOS/DBHelp/UserModels.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 class UserModels{
-    enum SubcriptionType:String, CaseIterable{
-         case prize = "awarded"
-        case trial = "trail"
-        case paid = "paid"
+    enum SubcriptionType:Int, CaseIterable{
+         case prize = 2
+        case trial = 1
+        case paid = 0
     
     }
     var Email:[String]
     var Password:String
-    var Subscript:String
+    var Subscript:Int
     var status:String
     
     //optional fields
@@ -33,20 +33,31 @@ class UserModels{
         status="unblocked"
         
     }
+    init( UserName:String, Password:String, DOB:String, admin:Bool, subriction:String, Status:String, First:String, Last:String, email:[String]){
+        self.UserName = UserName
+        self.Password = Password
+        self.Dob=DOB
+        self.admin=admin
+        self.Subscript=0
+        self.status = Status
+        self.First = First
+        self.Last = Last
+        self.Email = email
+    }
     init(ID:Int, UserName:String, Password:String, DOB:String, admin:Bool, subriction:String, Status:String, First:String, Last:String, email:[String]){
         self.ID=ID
         self.UserName = UserName
         self.Password = Password
         self.Dob=DOB
         self.admin=admin
-        self.Subscript=subriction
+        self.Subscript=0
         self.status = Status
         self.First = First
         self.Last = Last
         self.Email = email
     }
     init(Email:String, Password:String){
-        self.Email=[String]()
+        self.Email=[Email]
         self.Password=Password
         Subscript=SubcriptionType.trial.rawValue
         status="unblocked"
