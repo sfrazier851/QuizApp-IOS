9e2e67676f8867128cf2acf3fec501a4e64b99b0
Christopher Medina <chibi310@hotmail.com>
2022-03-21 10:10:38 -0400

    Added Quiz Creation Menu, Functionality coming soon...

---
+++ QuizApp-IOS/Controllers/Quiz_Editor.swift
@@ -0,0 +1,23 @@
+//
+//  Quiz_Editor.swift
+//  QuizApp-IOS
+//
+//  Created by Christopher Medina on 3/21/22.
+//
+
+import UIKit
+
+class Quiz_Editor: UIViewController {
+
+    @IBOutlet weak var Scroll: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+
+}
